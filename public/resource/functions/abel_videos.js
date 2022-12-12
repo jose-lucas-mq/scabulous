81c887cd419386322a65798b705e6894f4ed7cde
jose-lucas-mq <jose2002lucas@gmail.com>
2022-12-12 14:32:36 -0300

    hours, minutes, days and secs

--- public/resource/functions/abel_videos.js
+++ public/resource/functions/abel_videos.js
@@ -18,6 +18,12 @@ var videos = [
 ];
 
 window.onload = function () {
+    playVideo();
+    showDateShow();
+    setInterval(showDateShow, 1000);
+};
+
+function playVideo(){
     var playerDiv = document.getElementById("player_video");
     var player = document.createElement("source");
     var randomVideoPath = videos[Math.floor(Math.random() * videos.length)];
@@ -25,15 +31,24 @@ window.onload = function () {
     player.setAttribute('type', "video/mp4");
 
     playerDiv.appendChild(player);
+}
 
-    // count days to the show
-
+function showDateShow(){
     var actual_date = new Date();
-    var show_date = new Date("10/10/2023")
+    var show_date = new Date("10/10/2023 16:00:00")
 
-    var diff = (show_date.getTime() - actual_date.getTime())/864e5
+    var diff = (show_date.getTime() - actual_date.getTime())
+
+    var diff_days = Math.floor(diff / (1000 * 60 * 60 * 24))
+    var diff_hours = Math.floor(diff / 1000 / 60 / 60) % 24
+    var diff_minutes = Math.floor(diff / 1000 / 60 ) % 60
+    var diff_seconds = Math.floor(diff / 1000 ) % 60
+   
 
     var span_dias = document.getElementById("dias_pro_show");
 
-    span_dias.innerText = `${Math.round(diff)}`
-};
+    span_dias.innerHTML = `<br>${diff_days} dias <br>
+    ${diff_hours < 0 ? diff_hours * (-1) : diff_hours} hora(s) <br>
+    ${diff_minutes < 0 ? diff_minutes * (-1) : diff_minutes} minuto(s) <br>
+    e ${diff_seconds < 0 ? diff_seconds * (-1) : diff_seconds} segundo(s)<br>`
+}

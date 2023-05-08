99f0eefe253e589566c67cf8d09e90f890f6d7bf
jose-lucas-mq <jose2002lucas@gmail.com>
2023-05-08 14:07:31 -0300

    dor de cabeca

--- public/resource/functions/toscana.js
+++ public/resource/functions/toscana.js
@@ -7,7 +7,7 @@ function showDateHellEnd(){
     var actual_date = new Date();
     var show_date = new Date("04/12/2023 19:00:00")
 
-    var diff = (show_date.getTime() - actual_date.getTime())
+    var diff = (actual_date.getTime() - show_date.getTime())
 
     var diff_days = Math.floor(diff / (1000 * 60 * 60 * 24))
     var diff_hours = Math.floor(diff / 1000 / 60 / 60) % 24
